658de5fec158e4550a64fd161504b510dcd5e951
raybulous <raybulous@gmail.com>
2024-02-27 11:15:31 +0800

    Revert "Add files via upload"
    This reverts commit 06935f4871666f9a575795606c79770d286526a3.

--- bPlusTree/LeafNode.cs
+++ bPlusTree/LeafNode.cs
@@ -98,10 +98,4 @@ public class LeafNode<TKey, TValue> : Node<TKey, TValue> where TKey : IComparabl
         }
         return key;
     }
-
-    public override void DeleteRecord(long position)
-    {
-        //Yet to implement, need to delete record
-        //Find index of record, remove and update
-    }
 }
